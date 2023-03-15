ca6717c2cbfbd58c14cbe0b57cde8fcff50baa1e
Jack Kelly <jack@jckelly.xyz>
2023-03-14 20:36:49 -0700

    Semi functional UI loop

--- paradise.js
+++ paradise.js
@@ -1,18 +1,268 @@
+class Utils {
+
+	// https://dev.to/codebubb/how-to-shuffle-an-array-in-javascript-2ikj
+	static shuffle(array) {
+		for (let i = array.length - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			const temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+		}		  
+	}
+
+	static sample(array, n) {
+		let chosen = []
+		while (array.length > n && chosen.length < n) {
+			let index = Math.floor(Math.random() * array.length);
+			if (!chosen.includes(array[index])) {
+				chosen.push(array[index]);
+			}
+		}
+		return chosen ;
+	}
+}
+
+var displayStats = [
+	"confidence",
+	"mood",
+	"intoxication",
+	"attraction",
+	"friendship",
+]
+
+class UIManager {
+
+	constructor(onStep) {
+		let button = document.getElementById("stepButton");
+		button.onclick = onStep;
+	}
+
+	addEmptyCharacterCard(name) {
+		const characterTemplate = `
+	<div id="${name}" class="column character-container">
+		<div class="character-header column">
+			<div class="character-emoji"></div>
+			<div class="character-name"></div>
+		</div>
+		<div id="${name}-bio-container" class="column bio-container">
+			<div class="character-bio-label">
+				Bio
+			</div>
+			<div class="character-bio">
+			</div>
+		</div>
+		<div id="${name}-traits-container" class="column">
+			<div class="traits-label">
+				Traits
+			</div>
+			<div class="chauracter-traits">
+				<div class="character-traits-good">
+				</div>
+				<div class="character-traits-bad">
+				</div>
+			</div>
+		</div>
+		<div id="${name}-stats" class="column">
+			<div>Stats</div>
+			<div id="${name}-self" class="column">
+				<div>Personal</div>
+			</div>
+			<div id="${name}-relationships" class="column">
+				<div>Relationships</div>
+			</div>
+		</div>
+	</div>
+	`
+		let characterContainer = document.getElementById("characterContainer")
+		characterContainer.innerHTML = characterContainer.innerHTML + characterTemplate;
+	}
+
+	updateCharacterData(name, data) {
+		let character = document.getElementById(name);
+		let characterLabel = character.getElementsByClassName("character-name")[0];
+		characterLabel.innerHTML = data.displayName;
+		let characterEmoji = character.getElementsByClassName("character-emoji")[0];
+		characterEmoji.innerHTML = data.emoji;
+
+		let characterBio = character.getElementsByClassName("character-bio")[0];
+		characterBio.innerHTML = data.bio;
+
+		let goodTraits = character.getElementsByClassName("character-traits-good")[0];
+		data.goodTraits.forEach(element => {
+			let trait = document.createElement("div");
+			trait.innerHTML = element;
+			trait.className = "character-trait";
+			goodTraits.appendChild(trait);
+		});
+		let badTraits = character.getElementsByClassName("character-traits-bad")[0];
+		data.badTraits.forEach(element => {
+			let trait = document.createElement("div");
+			trait.innerHTML = element;
+			trait.className = "character-trait";
+			badTraits.appendChild(trait);
+		});
+	}
+
+	addRelationshipForCharacter(character, relationship) {
+		let relationshipContainer = document.getElementById(`${character}-relationships`);
+		let relationshipElement = document.createElement("div");
+		relationshipElement.id = `${character}-${relationship}-relationship`;
+		relationshipElement.className = "column";
+		relationshipContainer.appendChild(relationshipElement);
+		relationshipElement.innerHTML = `<div class="relationship-label">${relationship}</div>`;
+	}
+
+	addStatForCharacter(character, statName, displayType, recipient) {
+		let target = `${character}`
+		if (recipient) {
+			target = `${character}-${recipient}`
+		}
+		let id = `${target}-${statName}`
+
+		let innerTemplate = `
+		<div class="stat-valuelabel"></div>
+		<div class="stat-progressbar">
+			<div class="stat-progressbar-progress"></div>
+		</div>`;
+		if (displayType !== "numeric") {
+			innerTemplate = `
+			<div class="stat-valuelabel"></div>`;
+		}
+
+		let outerTemplate = `
+			<div class="stat-label"></div>
+			<div class="row stat-body">
+				${innerTemplate}
+			</div>`;
+
+		let stat =  document.createElement("div");
+		stat.className = "column stat-container"
+		stat.id = id
+		stat.innerHTML = outerTemplate;
+
+		stat.getElementsByClassName("stat-label")[0].innerHTML = statName;
+
+		let parent = document.getElementById(`${character}-self`);
+		if (recipient) {
+			parent = document.getElementById(`${character}-${recipient}-relationship`);
+		}
+		parent.appendChild(stat);
+		return stat;
+	}
+
+	getOrCreateStat(character, statName, displayType, recipient) {
+		let stat_id = `${character}-${statName}`;
+		if (recipient) {
+			stat_id = `${character}-${recipient}-${statName}`
+			let relationship = document.getElementById(`${character}-${recipient}-relationship`)
+			if (!relationship) {
+				this.addRelationshipForCharacter(character, recipient);
+			}
+		}
+		let stat = document.getElementById(stat_id);
+		if (!stat) {
+			stat =this.addStatForCharacter(character, statName, displayType, recipient)
+		}
+		return stat;
+	}
+
+	updateStatForCharacter(character, statName, value, displayType, recipient) {
+		let stat = this.getOrCreateStat(character, statName, displayType, recipient)
+
+		let valueLabel = stat.getElementsByClassName("stat-valuelabel")[0];
+		if (displayType !== "numeric") {
+			valueLabel.innerHTML = value
+		}
+		else {
+			valueLabel.innerHTML = `${value}/100`;
+			let progress = stat.getElementsByClassName("stat-progressbar-progress")[0];
+			progress.style.width = `${value}%`;
+		}
+	}
+
+	updateCharacter(character, stats) {
+		let personalStats = stats["self"]
+		for (const stat of displayStats) {
+			if (personalStats.hasOwnProperty(stat)) {
+				this.updateStatForCharacter(character, stat, personalStats[stat], "numeric");
+			}
+		}
+		let relationshipStats = stats["relationships"]
+		for (const person in relationshipStats) {
+			let relationship = relationshipStats[person];
+			for (const stat of displayStats) {
+				if (relationship.hasOwnProperty(stat)) {
+					this.updateStatForCharacter(character, stat, relationship[stat], "numeric", person);
+				}
+			}
+		}
+	}
+
+	updateCharacters(stats) {
+		for (const character in stats) {
+			this.updateCharacter(character, stats[character])
+		}
+	}
+
+	addCharacters(characters, bios, stats) {
+		for (const character of characters) {
+			this.addEmptyCharacterCard(character);
+			this.updateCharacterData(character, bios[character])
+			this.updateCharacter(character, stats[character])
+		}
+	}
+}
+
+
+class Game {
+	constructor(){
+		this.simulation = new Simulation(() => this.updateCharacters())
+		this.ui = new UIManager(() => this.step());
+	}
+
+	async initialize() {
+		return this.simulation.initialize()
+		.then(function () {
+			this.ui.addCharacters(
+				this.simulation.getCharacters(),
+				this.simulation.characterBios,
+				this.simulation.getState()
+			);
+		}.bind(this));
+	}
+
+	step() {
+		this.simulation.step()
+	}
+	
+	updateCharacters() {
+		let state = this.simulation.getState();
+		this.ui.updateCharacters(state);
+	}
+
+}
+
 class Simulation {
-	constructor() {
+	constructor(onCharacterUpdate) {
 		this.ensemble = ensemble;
 		this.prototypeSelf = {}
 		this.prototypeRelationship = {}
+		this.volitions = {}
+		this.turns = [],
+		this.onCharacterUpdate = onCharacterUpdate;
+		this.timer = 0;
+		this.characterFactory = new CharacterFactory();
+		this.characterBios = {}
 	}
 
 	async loadConfiguration() {
-		Promise.all([
-			fetch('./data/schema.json'),
-			fetch('./data/cast.json'),
-			fetch('./data/triggerRules.json'),
-			fetch('./data/volitionRules.json'),
-			fetch('./data/actions.json'),
-			fetch('./data/history.json'),
+		return Promise.all([
+			fetch('./schema/schema.json'),
+			fetch('./schema/cast.json'),
+			fetch('./schema/triggerRules.json'),
+			fetch('./schema/volitionRules.json'),
+			fetch('./schema/actions.json'),
+			fetch('./schema/history.json'),
 		]).then(
 			(responses) => Promise.all(responses.map((response) => response.json())))
 		.then(function (data) {
@@ -24,9 +274,11 @@ class Simulation {
 			this.ensemble.addRules(volitionRules);
 			this.ensemble.addActions(actions);
 			this.ensemble.addHistory(history);
-			this._loadStatePrototypes();
-			console.log(this.getState())
 		}.bind(this)); 
+	}
+
+	async loadCharacterFactory() {
+		return this.characterFactory.initialize();
 	}
 	
 	_loadStatePrototypes() {
@@ -63,16 +315,156 @@ class Simulation {
 		}
 		let socialRecord = this.ensemble.getSocialRecord();
 		for (const predicate of socialRecord[socialRecord.length - 1]) {
-			if (predicate.category in this.prototypeSelf) {
+			if (predicate.type in this.prototypeSelf) {
 				character_states[predicate.first]["self"][predicate.type] = predicate.value;
-			} else if (predicate.category in this.prototypeRelationship) {
-				character_states[predicate.first]["relationships"][predictate.second][predicate.type] = predicate.value;
+			} else if (predicate.type in this.prototypeRelationship) {
+				character_states[predicate.first]["relationships"][predicate.second][predicate.type] = predicate.value;
 			}
 		}
 		return character_states;
 	}
+
+	async initialize () {
+		return Promise.all([
+			this.loadConfiguration(),
+			this.loadCharacterFactory()
+		]).then(function () {
+			this._loadStatePrototypes();
+			this.volitions = this.ensemble.calculateVolition(this.ensemble.getCharacters())
+			// temp until character loading is finished
+			let state = this.getState();
+			for (const character of this.ensemble.getCharacters()) {
+				let charData = this.characterFactory.newCharacter(!state[character]["self"]["is_man"]);
+				this.characterBios[character] = charData;
+			}
+			this.resetTurns();
+		}.bind(this));
+	}
+
+	doAction(action) {
+		console.log(action);
+		this.ensemble.doAction(action);
+		this.ensemble.runTriggerRules(this.ensemble.getCharacters());
+		this.volitions = this.ensemble.calculateVolition(this.ensemble.getCharacters())
+	}
+
+	getActions(character) {
+		const intents = 5;
+		const actionsPerIntent = 10;
+		const actionsPerGroup = 10
+		let actionList = []
+		let characters = this.ensemble.getCharacters();
+		Utils.shuffle(characters);
+		for (const recipient of characters) {
+			let character_actions = this.ensemble.getActions(
+										character, 
+										recipient, 
+										this.volitions, 
+										characters, 
+										intents,
+										actionsPerIntent,
+										actionsPerGroup
+									);
+			actionList = actionList.concat(character_actions);
+		}
+		actionList.sort((a, b) => b.weight - a.weight);
+		return actionList;
+	}
+
+	takeTurn(character) {
+		console.log(`Taking turn as ${character}`)
+		let actions = this.getActions(character);
+		if (actions.length > 0) {
+			this.doAction(actions[0]);
+		}
+		this.onCharacterUpdate()
+	}
+
+	getCharacters() {
+		return this.ensemble.getCharacters();
+	}
+
+	step() {
+		if (this.turns.length > 0) {
+			let turn = this.turns.pop()
+			this.takeTurn(turn);
+		} else {
+			this.resetTurns();
+			this.nextTimeStep();
+		}
+	}
+
+	nextTimeStep() {
+		this.ensemble.setupNextTimeStep();
+	}
+
+	resetTurns() {
+		this.turns = this.ensemble.getCharacters();
+		Utils.shuffle(this.turns)
+	}
+
 }
 
-var game = new Simulation();
-game.loadConfiguration()
+class CharacterFactory {
+	// todo: gracefully handle running out of characters
+	constructor() {
+		this.maleNames = [];
+		this.femaleNames = [];
+		this.goodTraits = [];
+		this.badTraits = [];
+		this.skinTones = ["&#x1f3fb;", "&#x1f3fc;", "&#x1f3fd;", "&#x1f3fe;", "&#x1f3ff;"];
+		this.hairColors = ["&#x1f9b0;", "&#x1f9b1;", "&#x1f9b2;", "&#x1f9b3;", "&#xfe0f;"];
 
+	}
+
+	getEmoji(isFemale) {
+		let baseEmoji = "&#x1f468;";
+		if (isFemale) {
+			baseEmoji = "&#x1f469;";
+		}
+		let skinTone = Utils.sample(this.skinTones, 1)[0]
+		let zwj = "&#x200d;";
+		let hairColor = Utils.sample(this.hairColors, 1)[0]
+		return baseEmoji + skinTone + zwj + hairColor;
+	}
+
+	newCharacter(isFemale) {
+		let character = {}
+		if (isFemale) {
+			character["displayName"] = this.femaleNames.pop()
+		}
+		else {
+			character["displayName"] = this.maleNames.pop()
+		}
+		character["name"] = character["displayName"].toLowerCase();
+		character["goodTraits"] = Utils.sample(this.goodTraits, 2);
+		character["badTraits"] = Utils.sample(this.badTraits, 2);
+		character["bio"] = "Placeholder bio :)";
+		character["emoji"] = this.getEmoji(isFemale);
+		return character;
+	}
+
+	async initialize (){
+		return fetch('./data/characterFactory.json')
+		.then((response) => response.json())
+		.then(function (data) {
+			this.maleNames = data.male_names;
+			Utils.shuffle(this.maleNames);
+			this.femaleNames = data.female_names;
+			Utils.shuffle(this.femaleNames);
+			this.goodTraits = data.good_traits;
+			this.badTraits = data.bad_traits;
+		}.bind(this));
+	}
+}
+
+var game;
+
+document.onreadystatechange = function () {
+  if (document.readyState == "complete") {
+	game = new Game();
+	game.initialize().then(() => {
+		console.log("READY");
+	});
+  }
+}
