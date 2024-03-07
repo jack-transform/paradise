d6a8e78497765800abc720f4b0a172a4e99b973e
Jack Kelly <jack@jckelly.xyz>
2024-03-06 22:27:06 -0800

    updating the model to a non-deprecated one

--- src/open_ai.js
+++ src/open_ai.js
@@ -1,59 +1,63 @@
 class OpenAI {
-
     constructor() {
         this.API_KEY = "";
         this.OpenAI = null;
     }
-    
+
     setKey(key) {
         this.API_KEY = key;
         this.OpenAI = axios.create({
-            baseURL: 'https://api.openai.com/v1/',
+            baseURL: "https://api.openai.com/v1/",
             timeout: 100000,
             headers: {
-              'Content-Type': 'application/json',
-              'Access-Control-Allow-Origin': "*",
-              'Authorization': 'Bearer ' + key
-            }
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+                Authorization: "Bearer " + key,
+            },
         });
     }
     async initialize() {
-        return fetch('./data/.secrets.json')
-        .then((response) => response.json())
-        .then(function (json) {
-            this.setKey(key);
-        }.bind(this));
+        return fetch("./data/.secrets.json")
+            .then((response) => response.json())
+            .then(
+                function (json) {
+                    this.setKey(key);
+                }.bind(this)
+            );
     }
 
     async gptRequest(prompt) {
-        return this.gptRequestHelper(prompt, 3)
+        return this.gptRequestHelper(prompt, 3);
     }
 
     async gptRequest(prompt, retries) {
         return this.OpenAI({
-            "method": "post",
-            "url": "/completions",
-            "data": {
-                "model": "text-davinci-003",
-                "prompt": prompt,
-                "max_tokens": 200,
-                "top_p": 1,
-                "n": 1,
-                "stream": false,
-                "logprobs": null,
-                "stop": "\n"
+            method: "post",
+            url: "/completions",
+            data: {
+                model: "gpt-3.5-turbo-instruct",
+                prompt: prompt,
+                max_tokens: 200,
+                top_p: 1,
+                n: 1,
+                stream: false,
+                logprobs: null,
+                stop: "\n",
             },
-        }).catch(function (error) {
-            console.log(error.toJSON());
-        }).then(function(response) {
-            if (response.status === 200) {
-                return response;
-            } else if (retries > 0) {
-                return wait(1000).then(() => gptRequest(prompt, retries - 1));
-            }
-        });
+        })
+            .catch(function (error) {
+                console.log(error.toJSON());
+            })
+            .then(function (response) {
+                if (response.status === 200) {
+                    return response;
+                } else if (retries > 0) {
+                    return wait(1000).then(() =>
+                        gptRequest(prompt, retries - 1)
+                    );
+                }
+            });
     }
-
 }
 
 export default OpenAI;
